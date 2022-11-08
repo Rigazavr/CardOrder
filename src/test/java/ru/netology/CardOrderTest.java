1f3f0b85afa2f8b2aabc5ebd552e640469358624
Регина Васильева <skafest123@gmail.com>
2022-11-08 21:57:39 +0700

    v4

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -1,82 +1,81 @@
 package ru.netology;
 
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeEach;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
-public class CardOrderTest {
-
-    @BeforeEach
-    void setUp() {
-        open("http://localhost:9999");
-
+public class CardOrderTest{
+    @Test
+    void shouldSubmitRequest() {
+        open("http://localhost:9999/");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Иванов Иван");
+        form.$("[data-test-id=phone] input").setValue("+79133056879");
+        form.$("[data-test-id=agreement]").click();
+        form.$("button.button").click();
+        $("[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
     @Test
-    void shouldSuccessfully() {
+    void shouldSendRequestInvalidName() {
 
-        Configuration.holdBrowserOpen = false;
-
-        $x("//*[@name=\"name\"]").setValue("Регина");
-        $x("//*[@name=\"phone\"]").setValue("+79133056879");
-        $x("//*[@data-test-id=\"agreement\"]").click();
-        $x("//*[@type=\"button\"]").click();
-        $(withText("Ваша заявка успешно отправлена!")).shouldHave(visible);
+        open("http://localhost:9999/");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Ivanov Ivan");
+        form.$("[data-test-id=phone] input").setValue("+79133056879");
+        form.$("[data-test-id=agreement]").click();
+        form.$("button.button").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
-    void shouldNotCorrectName() {
+    void shouldSendRequestInvalidPhoneNumber() {
 
-        Configuration.holdBrowserOpen = false;
-
-        $x("//*[@data-test-id=\"agreement\"]").click();
-        $x("//*[@name=\"name\"]").setValue("Regina Vasileva");
-        $x("//*[@name=\"phone\"]").setValue("+7133056879");
-        $x("//*[@type=\"button\"]").click();
-        $(withText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."))
-                .shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        open("http://localhost:9999/");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Иванов Иван");
+        form.$("[data-test-id=phone] input").setValue("+7913305687");
+        form.$("[data-test-id=agreement]").click();
+        form.$("button.button").click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
-    void shouldEmptyNameField() {
+    void shouldSendRequestWithoutPhoneNumber() {
 
-        Configuration.holdBrowserOpen = false;
-
-        $x("//*[@data-test-id=\"agreement\"]").click();
-        $x("//*[@name=\"name\"]").setValue("");
-        $x("//*[@name=\"phone\"]").setValue("+79133056879");
-        $x("//*[@type=\"button\"]").click();
-        $(withText("Поле обязательно для заполнения")).shouldHave(exactText("Поле обязательно для заполнения"));
+        open("http://localhost:9999/");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Иванов Иван");
+        form.$("[data-test-id=phone] input").setValue("");
+        form.$("[data-test-id=agreement]").click();
+        form.$("button.button").click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
     }
 
     @Test
-    void shouldNotCorrectNamePhone() {
+    void shouldSendRequestWithoutName() {
 
-        Configuration.holdBrowserOpen = false;
-
-        $x("//*[@data-test-id=\"agreement\"]").click();
-        $x("//*[@name=\"name\"]").setValue("Регина");
-        $x("//*[@name=\"phone\"]").setValue("Regina");
-        $x("//*[@type=\"button\"]").click();
-        $(withText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."))
-                .shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        open("http://localhost:9999/");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("");
+        form.$("[data-test-id=phone] input").setValue("+79133056879");
+        form.$("[data-test-id=agreement]").click();
+        form.$("button.button").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
     }
 
     @Test
-    void shouldEmptyPhoneField() {
+    void shouldSendRequestWithoutCheckbox() {
 
-        Configuration.holdBrowserOpen = false;
+        open("http://localhost:9999/");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Иванов Иван");
+        form.$("[data-test-id=phone] input").setValue("+79133056879");
+        form.$("button.button").click();
+        form.$("[data-test-id=agreement]").should(Condition.cssClass("input_invalid"));
 
-        $x("//*[@data-test-id=\"agreement\"]").click();
-        $x("//*[@name=\"name\"]").setValue("Антон");
-        $x("//*[@name=\"phone\"]").setValue("");
-        $x("//*[@type=\"button\"]").click();
-        $(withText("Поле обязательно для заполнения"))
-                .shouldHave(exactText("Поле обязательно для заполнения"));
     }
 }
